c21e458156b40a2f7b0ee826609e44543bb8092f
lavanthreddy11 <lavanthreddy11@gmail.com>
2025-11-03 12:59:48 +0530

    Update terraform.tf

--- terraform/apps/terraform.tf
+++ terraform/apps/terraform.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0" # 👈 downgrade to 5.x
+      version = "~> 5.0"
     }
     helm = {
       source  = "hashicorp/helm"
       version = ">=2.17"
     }
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
       version = "2.37.1"
     }
   }
@@ -27,4 +27,49 @@ provider "helm" {
 
 provider "kubernetes" {
   config_path = "~/.kube/config"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.1"     # ✅ compatible with AWS provider 5.x
+
+  name            = "my-vpc"
+  cidr            = "10.0.0.0/16"
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.21"   # ✅ stable EKS module for AWS provider 5.x
+
+  cluster_name    = "observability"
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      max_size     = 3
+      min_size     = 1
+
+      instance_types = ["t3.medium"]
+    }
+  }
 }

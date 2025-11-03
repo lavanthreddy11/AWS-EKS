9efd4e32718d5a1e81ff39ccc1ef9146a9941e0f
lavanthreddy11 <lavanthreddy11@gmail.com>
2025-11-03 13:09:43 +0530

    Update terraform.tf

--- terraform/apps/terraform.tf
+++ terraform/apps/terraform.tf
@@ -1,18 +1,32 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.57.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.30"
+      version = "~> 2.30.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.12"
+      version = "~> 2.12.0"
     }
   }
+}
 
-  required_version = ">= 1.3.0"
+provider "aws" {
+  region = "ap-south-1"
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
 }
